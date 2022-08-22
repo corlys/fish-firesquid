12f7bc6744b1f4a0d6a15ba4a7048686b75d8a18
corlys <duscae047@gmail.com>
2022-08-22 10:41:01 +0700

    feat: testing secret update

--- db/migrations/1661135767231-Data.js
+++ db/migrations/1661135767231-Data.js
@@ -1,5 +1,5 @@
-module.exports = class Data1660893029996 {
-  name = 'Data1660893029996'
+module.exports = class Data1661135767231 {
+  name = 'Data1661135767231'
 
   async up(db) {
     await db.query(`CREATE TABLE "owner" ("id" character varying NOT NULL, "balance" numeric, CONSTRAINT "PK_8e86b6b9f94aece7d12d465dc0c" PRIMARY KEY ("id"))`)
